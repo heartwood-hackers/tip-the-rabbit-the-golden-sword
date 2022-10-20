b7f36d40377061af626c98f0b0d4d0e8ede33208
Loren Norman <lorennorman@gmail.com>
2022-10-19 23:07:35 -0400

    restore some turtle animations

--- characters/Character.gd
+++ characters/Character.gd
@@ -8,6 +8,8 @@ export var player_number=1
 var previous_velocity
 var current_velocity
 
+onready var animation_player = $AnimationPlayer
+
 func _on_body_entered(body):
   if(body.is_in_group("boundaries")): body._collide_with_character(self)
 
@@ -25,13 +27,19 @@ func _physics_process(_delta):
   current_velocity = self.linear_velocity
 
 func _integrate_forces(state):
-  var right = Input.get_action_strength("move_right%s"%player_number)*speed
-  var left = Input.get_action_strength("move_left%s"%player_number)*speed
+  var right = strength("move_right")*speed
+  var left = strength("move_left")*speed
   var lateral_velocity=linear_velocity.x if (right-left==0) else right-left
-  var jumped = Input.is_action_just_pressed("jump%s"%player_number)
+  var jumped = pressed("jump", true)
   var up_velocity = -jump_speed if jumped else linear_velocity.y
   
   state.linear_velocity = Vector2(lateral_velocity, up_velocity)
+
+func strength(action: String):
+  return Input.get_action_strength("%s%s" % [action, player_number])
+
+func pressed(action: String, just: bool=false):
+  return Input.is_action_just_pressed("%s%s" % [action, player_number]) if just else Input.is_action_pressed("%s%s" % [action, player_number])
 
 func _process(_delta):
   if $Animations:
@@ -40,23 +48,23 @@ func _process(_delta):
     if linear_velocity.x > 1:
       $Animations.flip_h = false
 
-#  if !$AnimationPlayer: return
-#
-#  if $AnimationPlayer.current_animation != "Melee" and $AnimationPlayer.current_animation != "Throw":
-#    if Input.is_action_pressed("move_left2") or Input.is_action_pressed("move_right2"):
-#      $AnimationPlayer.play("Run")
-#
-#    if Input.is_action_just_pressed("jump2"):
-#      $AnimationPlayer.play("Lift Off")
-#
-#    if Input.is_action_just_pressed("attack_melee2"):
-#      $AnimationPlayer.play("Melee")
-#
-#    if Input.is_action_just_pressed("attack_range2"):
-#      $AnimationPlayer.play("Throw")
-#
-#  if !$AnimationPlayer.is_playing():
-#    if linear_velocity.y != 0:
-#      $AnimationPlayer.play("Air Idle")
-#    else:
-#      $AnimationPlayer.play("Idle")
+  if !animation_player: return
+
+  if $AnimationPlayer.current_animation != "Melee" and $AnimationPlayer.current_animation != "Throw":
+    if pressed("move_left") or pressed("move_right"):
+      $AnimationPlayer.play("Run")
+
+    if pressed("jump"):
+      $AnimationPlayer.play("Lift Off")
+
+    if pressed("attack_melee", true):
+      $AnimationPlayer.play("Melee")
+
+    if pressed("attack_range", true):
+      $AnimationPlayer.play("Throw")
+
+  if !$AnimationPlayer.is_playing():
+    if linear_velocity.y != 0:
+      $AnimationPlayer.play("Air Idle")
+    else:
+      $AnimationPlayer.play("Idle")
