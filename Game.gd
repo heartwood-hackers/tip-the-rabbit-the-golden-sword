2d875473fe6f3442cb5305a2429658202421d6e2
legojida <116694830+legojida@users.noreply.github.com>
2022-10-26 10:28:10 -0400

    fix whitespace

--- Game.gd
+++ Game.gd
@@ -9,9 +9,9 @@ func _ready():
     var level_num = rng.randi_range(0, 1)
 
     if level_num == 0:
-      level = load("res://levels/cyberpunk_street/CyberpunkStreet.tscn")
+        level = load("res://levels/cyberpunk_street/CyberpunkStreet.tscn")
     elif level_num == 1:
-      level = load("res://levels/glacial_mountain/GlacialMountain.tscn")
-  
+        level = load("res://levels/glacial_mountain/GlacialMountain.tscn")
+
   var level_instance = level.instance()
   $Level.add_child(level_instance)
