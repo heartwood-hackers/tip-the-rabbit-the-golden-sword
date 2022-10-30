ef77d848ecc01562191f9d997bf8918f597ff549
Loren Norman <lorennorman@gmail.com>
2022-10-30 15:10:14 -0400

    player cards and input stuff

---
+++ PlayerCard.gd
@@ -0,0 +1,78 @@
+extends Control
+
+var input = preload("res://util/input_helper.gd")
+
+# input player number attached to this card
+export(int, 1, 4) var player_number = 1
+
+# modes and transitions
+enum {Attract, Select, Active}
+export var mode = Attract
+func _set_mode(new_mode: int):
+  mode = new_mode
+  for child in get_children():
+    child.visible = false
+
+  if(mode == Attract):
+    $AttractMode.visible = true
+  elif(mode == Select):
+    $SelectMode.visible = true
+  elif(mode == Active):
+    $ActiveMode.visible = true
+
+func _ready():
+  self._set_mode(mode)
+
+  print("registering %s"%player_number)
+  # register input listeners
+  if(player_number == 1 or player_number == 2):
+    input.add_player_action(player_number, "move_right", [
+      { "device": player_number, "axis": -JOY_AXIS_1 }
+    ])
+    input.add_player_action(player_number, "move_left", [
+      { "device": player_number, "axis": JOY_AXIS_1 }
+    ])
+    input.add_player_action(player_number, "jump", [
+      { "device": player_number, "button": JOY_XBOX_A }
+    ])
+    input.add_player_action(player_number, "attack_melee", [
+      { "device": player_number, "button": JOY_XBOX_X }
+    ])
+    input.add_player_action(player_number, "attack_range", [
+      { "device": player_number, "button": JOY_XBOX_B }
+    ])
+
+  elif(player_number == 3):
+    input.add_player_action(player_number, "move_right", [
+      { "scancode": KEY_D }
+    ])
+    input.add_player_action(player_number, "move_left", [
+      { "scancode": KEY_A }
+    ])
+    input.add_player_action(player_number, "jump", [
+      { "scancode": KEY_W }, { "scancode": KEY_V }
+    ])
+    input.add_player_action(player_number, "attack_melee", [
+      { "scancode": KEY_B }
+    ])
+    input.add_player_action(player_number, "attack_range", [
+      { "scancode": KEY_N }
+    ])
+
+  elif(player_number == 4):
+    input.add_player_action(player_number, "move_right", [
+      { "scancode": KEY_RIGHT }
+    ])
+    input.add_player_action(player_number, "move_left", [
+      { "scancode": KEY_LEFT }
+    ])
+    input.add_player_action(player_number, "jump", [
+      { "scancode": KEY_UP }
+    ])
+    input.add_player_action(player_number, "attack_melee", [
+      { "scancode": KEY_QUESTION }
+    ])
+    input.add_player_action(player_number, "attack_range", [
+      { "scancode": KEY_PERIOD }
+    ])
+
---
+++ util/input_helper.gd
@@ -0,0 +1,30 @@
+extends Node
+
+
+static func add_player_action(player_number, action_name, events):
+  var player_action = "p%s_%s" % [player_number, action_name]
+  InputMap.add_action(player_action)
+  
+  for event in events:
+    if !event: continue # allow sparse event lists, prettier dsl
+
+    var input_event
+    if event.has("device"):
+      if event.has("axis"):
+        input_event = InputEventJoypadMotion.new()
+        input_event.axis = event["axis"]
+      elif event.has("button"):
+        input_event = InputEventJoypadButton.new()
+        input_event.button_index = event["button"]
+      else:
+        printerr("Bad input: %s %s %s" % [player_number, action_name, event])
+      input_event.device = event["device"]
+    
+    elif event.has("scancode"):
+      input_event = InputEventKey.new()
+      input_event.scancode = event["scancode"]
+    else:
+      printerr("Bad input: %s %s %s" % [player_number, action_name, event])
+
+    if input_event:
+      InputMap.action_add_event(player_action, input_event)
