9f3405e8e856107b7f715dd5ee9fc567df992393
Loren Norman <lorennorman@gmail.com>
2022-11-07 14:52:56 -0500

    try walls as area2d

--- levels/Wall.gd
+++ levels/Wall.gd
@@ -1,4 +1,10 @@
-extends RigidBody2D
+extends Area2D
 
 func _collide_with_character(character: Character):
-  character.apply_central_impulse(Vector2(0, -80))
+  var lateral_velocity = character.linear_velocity.x
+  character.apply_central_impulse(Vector2(-lateral_velocity*.75, -80))
+
+
+func _on_body_entered(body):
+  if(body.is_in_group("Characters")):
+    _collide_with_character(body as Character)
